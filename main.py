25a68207839bb97547b8cca1c2161b4c58efec20
Kritik Bangera <bangerakritik@gmail.com>
2025-03-13 13:59:24 +0000

    Second Commit

--- main.py
+++ main.py
@@ -5,3 +5,24 @@ import os
 load_dotenv()
 
 client = MemoryClient(api_key=os.environ.get("MEM0_API"))
+
+config = {
+    "vector_store": {
+        "provider": "supabase",
+        "config": {
+            "connection_string": "postgresql://user:password@host:port/database",
+            "collection_name": "memories",
+            "index_method": "hnsw",  # Optional: defaults to "auto"
+            "index_measure": "cosine_distance"  # Optional: defaults to "cosine_distance"
+        }
+    }
+}
+
+messages = [
+    {"role": "user", "content": "Hi, I'm Kritik. I'm a Non vegetarian and I'm allergic to squids."},
+    {"role": "assistant", "content": "Hello Kritik! I've noted that you're a Non vegetarian and have a squid allergy. I'll keep this in mind for any food-related recommendations or discussions."}
+]
+client.add(messages, user_id="kritik")
+
+# query = "What can I cook for dinner tonight?"
+# client.search(query, user_id="kritik")
